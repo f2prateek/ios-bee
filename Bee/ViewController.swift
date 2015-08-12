ad91bb261687bf0a12d228f46bb9514db66c5bdc
Prateek Srivastava <f2prateek@gmail.com>
2015-08-12 02:24:48 -0700

    Initial commit

---
+++ Bee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bee
+//
+//  Created by Prateek Srivastava on 2015-08-12.
+//  Copyright (c) 2015 f2prateek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,4 @@
+use_frameworks!
+platform :ios, '8.0'
+
+pod 'ReactiveCocoa', '~> 3.0-RC.1'

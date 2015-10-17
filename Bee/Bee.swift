bc1750686040c135e6f4953c054c43e29f49117f
Prateek Srivastava <f2prateek@gmail.com>
2015-10-17 01:08:39 -0700

    Update code styles

--- Bee/Bee.swift
+++ Bee/Bee.swift
@@ -1,44 +1,44 @@
 import Foundation
 
 public class Bee {
-  
+
   static let Table: [Character:String] = [
-    "a": "Alfa",
-    "b": "Bravo",
-    "c": "Charlie",
-    "d": "Delta",
-    "e": "Echo",
-    "f": "Foxtrot",
-    "g": "Golf",
-    "h": "Hotel",
-    "i": "India",
-    "j": "Juliett",
-    "k": "Kilo",
-    "l": "Lima",
-    "m": "Mike",
-    "n": "November",
-    "o": "Oscar",
-    "p": "Papa",
-    "q": "Quebec",
-    "r": "Romeo",
-    "s": "Sierra",
-    "t": "Tango",
-    "u": "Uniform",
-    "v": "Victor",
-    "w": "Whiskey",
-    "x": "X-ray",
-    "y": "Yankee",
-    "z": "Zulu",
-    "1": "One",
-    "2": "Two",
-    "3": "Three",
-    "4": "Four",
-    "5": "Five",
-    "6": "Six",
-    "7": "Seven",
-    "8": "Eight",
-    "9": "Nine",
-    "0": "Zero"
+      "a": "Alfa",
+      "b": "Bravo",
+      "c": "Charlie",
+      "d": "Delta",
+      "e": "Echo",
+      "f": "Foxtrot",
+      "g": "Golf",
+      "h": "Hotel",
+      "i": "India",
+      "j": "Juliett",
+      "k": "Kilo",
+      "l": "Lima",
+      "m": "Mike",
+      "n": "November",
+      "o": "Oscar",
+      "p": "Papa",
+      "q": "Quebec",
+      "r": "Romeo",
+      "s": "Sierra",
+      "t": "Tango",
+      "u": "Uniform",
+      "v": "Victor",
+      "w": "Whiskey",
+      "x": "X-ray",
+      "y": "Yankee",
+      "z": "Zulu",
+      "1": "One",
+      "2": "Two",
+      "3": "Three",
+      "4": "Four",
+      "5": "Five",
+      "6": "Six",
+      "7": "Seven",
+      "8": "Eight",
+      "9": "Nine",
+      "0": "Zero"
   ]
 
   public static func spell(text: String) -> String {
@@ -46,26 +46,26 @@ public class Bee {
 
     var addSpace = true
     for char in text.lowercaseString {
-        if char == " " {
-            if count(spelt) > 0 {
-                spelt = spelt + "\n"
-                addSpace = false
-            }
-            continue
+      if char == " " {
+        if count(spelt) > 0 {
+          spelt = spelt + "\n"
+          addSpace = false
         }
+        continue
+      }
 
-        if count(spelt) > 0 && addSpace {
-            spelt = spelt + " "
-        }
-        addSpace = true
+      if count(spelt) > 0 && addSpace {
+        spelt = spelt + " "
+      }
+      addSpace = true
 
-        var a:String?
-        a = Bee.Table[char]
-        if a == nil {
-            a = String(char)
-        }
+      var a: String?
+      a = Bee.Table[char]
+      if a == nil {
+        a = String(char)
+      }
 
-        spelt = spelt + a!
+      spelt = spelt + a!
     }
 
     return spelt
--- Bee/ViewController.swift
+++ Bee/ViewController.swift
@@ -5,25 +5,29 @@ class ViewController: UIViewController {
 
   @IBOutlet weak var editor: UITextField!
   @IBOutlet weak var display: UILabel!
-  
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    
+
     let input = editor
-      .rac_textSignal()
-      .toSignalProducer()
-      |> skip(1) // Skip first empty string ""
-      |> map { text in text as! String }
-      |> throttle(0.5, onScheduler: QueueScheduler.mainQueueScheduler)
-    
-    input.start(next: { text in
-      self.display.text = Bee.spell(text)
-    })
+    .rac_textSignal()
+    .toSignalProducer()
+        |> skip(1) // Skip first empty string ""
+        |> map {
+            text in text as! String
+        }
+        |> map { text in
+            return self.trim(text)
+        }
+        |> throttle(0.5, onScheduler: QueueScheduler.mainQueueScheduler)
+        |> observeOn(UIScheduler())
+        |> start { text in
+            self.display.text = Bee.spell(text)
+        }
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
+  private func trim(text: String) -> String {
+    return text.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
   }
-
 }
 
